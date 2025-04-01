960302e75eac5b293fd9761ff062324d7daec911
Egor_Sysoev <MrTFLOPS@yandex.com>
2025-03-31 21:12:12 -0700

    внес правки по checkstyle

--- src/main/TaskManager.java
+++ src/main/TaskManager.java
@@ -6,16 +6,24 @@ import model.Subtask;
 import java.util.List;
 
 public interface TaskManager {
+
     Task getTask(int id);
+
     Epic getEpic(int id);
+
     Subtask getSubtask(int id);
+
     List<Task> getHistory();
 
     void addTask(Task task);
+
     void addEpic(Epic epic);
+
     void addSubtask(Subtask subtask);
 
     void deleteTaskById(int id);
+
     List<Task> getAllTasks();
+
     void deleteAllTasks();
 }
