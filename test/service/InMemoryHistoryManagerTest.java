a4827f2f7efc1024c6b496337efc384588268918
Egor_Sysoev <MrTFLOPS@yandex.com>
2025-04-01 00:17:41 -0700

    Sprint 6

--- test/service/InMemoryHistoryManagerTest.java
+++ test/service/InMemoryHistoryManagerTest.java
@@ -1,45 +1,47 @@
+package service;
+
 import main.InMemoryHistoryManager;
-import model.Status;
 import model.Task;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class InMemoryHistoryManagerTest {
 
+    private InMemoryHistoryManager historyManager;
+
+    @BeforeEach
+    public void setUp() {
+        historyManager = new InMemoryHistoryManager();
+    }
+
     @Test
-    void testAddAndRetrieveHistory() {
-        InMemoryHistoryManager historyManager = new InMemoryHistoryManager();
+    public void testAddTaskToHistory() {
+        Task task = new Task(1, "Test Task", "Description", Status.NEW);
+        historyManager.add(task);
 
-        Task task1 = new Task("Task 1", "Description 1", Status.NEW);
-        Task task2 = new Task("Task 2", "Description 2", Status.IN_PROGRESS);
+        assertEquals(1, historyManager.getHistory().size());
+        assertEquals(task, historyManager.getHistory().get(0));
+    }
 
+    @Test
+    public void testRemoveTaskFromHistory() {
+        Task task = new Task(1, "Test Task", "Description", Status.NEW);
+        historyManager.add(task);
+        historyManager.remove(1);
+
+        assertTrue(historyManager.getHistory().isEmpty());
+    }
+
+    @Test
+    public void testGetHistoryOrder() {
+        Task task1 = new Task(1, "Task 1", "Description", Status.NEW);
+        Task task2 = new Task(2, "Task 2", "Description", Status.NEW);
         historyManager.add(task1);
         historyManager.add(task2);
 
         assertEquals(2, historyManager.getHistory().size());
         assertEquals(task1, historyManager.getHistory().get(0));
         assertEquals(task2, historyManager.getHistory().get(1));
-    }
-
-    @Test
-    void testDuplicateRemoval() {
-        InMemoryHistoryManager historyManager = new InMemoryHistoryManager();
-
-        Task task = new Task("Task 1", "Description", Status.NEW);
-        historyManager.add(task);
-        historyManager.add(task);
-
-        assertEquals(1, historyManager.getHistory().size());
-    }
-
-    @Test
-    void testRemoveFromHistory() {
-        InMemoryHistoryManager historyManager = new InMemoryHistoryManager();
-
-        Task task = new Task("Task", "Description", Status.NEW);
-        historyManager.add(task);
-        historyManager.remove(task.getId());
-
-        assertEquals(0, historyManager.getHistory().size());
     }
 }
