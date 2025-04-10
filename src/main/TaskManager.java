37793b801f6c0293d59ce8547c15f502dab6ab02
Сысоев Егор Александрович <SysoevEA@hq.corp.mos.ru>
2025-04-10 15:58:31 +0300

    10/04/ чекстайл taskmanager

--- src/main/TaskManager.java
+++ src/main/TaskManager.java
@@ -6,30 +6,47 @@ import java.util.List;
 public interface TaskManager {
     // Методы для задач
     List<Task> getAllTasks();
+
     Task getTask(int id);
+
     void addTask(Task task);
+
     void updateTask(Task task);
+
     void deleteTask(int id);
+
     void deleteAllTasks();
 
     // Методы для подзадач
     List<Subtask> getAllSubtasks();
+
     Subtask getSubtask(int id);
+
     void addSubtask(Subtask subtask);
+
     void updateSubtask(Subtask subtask);
+
     void deleteSubtask(int id);
+
     void deleteAllSubtasks();
+
     List<Subtask> getEpicSubtasks(int epicId);
 
     // Методы для эпиков
     List<Epic> getAllEpics();
+
     Epic getEpic(int id);
+
     void addEpic(Epic epic);
+
     void updateEpic(Epic epic);
+
     void deleteEpic(int id);
+
     void deleteAllEpics();
 
     // Общие методы
     List<Task> getHistory();
+
     List<Task> getPrioritizedTasks();
 }
