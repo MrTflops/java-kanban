b1f3c38bf98b5c743956aea810d8aa41b378d7c8
Сысоев Егор Александрович <SysoevEA@hq.corp.mos.ru>
2025-04-10 17:50:13 +0300

    чекстайл

--- src/main/TaskManager.java
+++ src/main/TaskManager.java
@@ -31,12 +31,12 @@ public interface TaskManager {
     void deleteAllSubtasks();
 
     List<Subtask> getSubtaskListByEpicId(int epicId);
-    // Добавлен новый метод
+
     List<Subtask> getEpicSubtasks(int epicId);
 
     // Методы для эпиков
     List<Epic> getEpicList();
-    // Добавлен новый метод
+
     List<Epic> getAllEpics();
 
     Epic getEpic(int id);
@@ -48,7 +48,7 @@ public interface TaskManager {
     void deleteEpic(int id);
 
     void deleteEpicById(int id);
-    // Добавлен новый метод
+
     void deleteAllEpics();
 
     // Общие методы
