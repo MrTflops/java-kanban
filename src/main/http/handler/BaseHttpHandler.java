5063671e475ab936a6a3ce6395196970e609f66c
Сысоев Егор Александрович <SysoevEA@hq.corp.mos.ru>
2025-04-10 16:09:15 +0300

    10/04/ чекстайл taskmanager

--- src/main/http/handler/BaseHttpHandler.java
+++ src/main/http/handler/BaseHttpHandler.java
@@ -1,33 +1,58 @@
-package main.http.handler;
+package main;
 
-import com.sun.net.httpserver.HttpHandler;
+import com.google.gson.Gson;
 import com.sun.net.httpserver.HttpExchange;
-import main.TaskManager;
+import com.sun.net.httpserver.HttpHandler;
+import main.http.HttpTaskServer;
+
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.OutputStream;
 import java.nio.charset.StandardCharsets;
 
 public abstract class BaseHttpHandler implements HttpHandler {
     protected final TaskManager taskManager;
-    protected final Gson gson = new Gson();
+    protected final Gson gson;
 
     protected BaseHttpHandler(TaskManager taskManager) {
         this.taskManager = taskManager;
+        this.gson = HttpTaskServer.getGson();
     }
 
-    protected void sendResponse(HttpExchange exchange, String response, int statusCode) throws IOException {
-        exchange.getResponseHeaders().set("Content-Type", "application/json; charset=UTF-8");
-        byte[] responseBytes = response.getBytes(StandardCharsets.UTF_8);
-        exchange.sendResponseHeaders(statusCode, responseBytes.length);
-        try (OutputStream os = exchange.getResponseBody()) {
-            os.write(responseBytes);
-        }
+    protected void sendText(HttpExchange exchange, String text, int statusCode) throws IOException {
+        byte[] resp = text.getBytes(StandardCharsets.UTF_8);
+        exchange.getResponseHeaders().add("Content-Type", "application/json;charset=utf-8");
+        exchange.sendResponseHeaders(statusCode, resp.length);
+        exchange.getResponseBody().write(resp);
+        exchange.close();
+    }
+
+    protected void sendSuccess(HttpExchange exchange, String response) throws IOException {
+        sendText(exchange, response, 200);
+    }
+
+    protected void sendCreated(HttpExchange exchange, String response) throws IOException {
+        sendText(exchange, response, 201);
+    }
+
+    protected void sendNotFound(HttpExchange exchange) throws IOException {
+        sendText(exchange, "Задача не найдена", 404);
+    }
+
+    protected void sendNotAcceptable(HttpExchange exchange, String message) throws IOException {
+        sendText(exchange, message, 406);
+    }
+
+    protected void sendInternalError(HttpExchange exchange) throws IOException {
+        sendText(exchange, "Внутренняя ошибка сервера", 500);
     }
 
     protected String readRequestBody(HttpExchange exchange) throws IOException {
-        try (InputStream is = exchange.getRequestBody()) {
-            return new String(is.readAllBytes(), StandardCharsets.UTF_8);
+        try (InputStream inputStream = exchange.getRequestBody()) {
+            return new String(inputStream.readAllBytes(), StandardCharsets.UTF_8);
         }
+    }
+
+    protected <T> T parseRequestBody(String body, Class<T> clazz) {
+        return gson.fromJson(body, clazz);
     }
 }
